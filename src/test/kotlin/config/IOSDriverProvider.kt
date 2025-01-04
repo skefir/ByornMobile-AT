a4b89a6e60262fc3fc7aad52618b63ee9badad0d
Alexander Manakov <novskt@gmail.com>
2025-01-04 15:25:09 +0300

    add parametrs for ios

--- src/test/kotlin/config/IOSDriverProvider.kt
+++ src/test/kotlin/config/IOSDriverProvider.kt
@@ -7,6 +7,8 @@ import org.openqa.selenium.Capabilities
 import org.openqa.selenium.WebDriver
 import org.skefir.common.DataConstants
 import org.skefir.common.DataConstants.APPIUM_URL
+import org.skefir.common.DataConstants.IOS_DEVICE_NAME
+import org.skefir.common.DataConstants.IOS_PLATFORM_VERSION
 import java.net.MalformedURLException
 import java.net.URI
 import javax.annotation.CheckReturnValue
@@ -24,8 +26,8 @@ object IOSDriverProvider : WebDriverProvider {
         options.merge(capabilities)
         options.setAutomationName("XCUITest")
         options.setPlatformName("iOS")
-        options.setDeviceName("iPhone 16 Pro")
-        options.setPlatformVersion("18.1")
+        options.setDeviceName(IOS_DEVICE_NAME)
+        options.setPlatformVersion(IOS_PLATFORM_VERSION)
         options.setApp(DataConstants.IOS_INSTALL_PATH)
         options.setCapability("xcodeSigningId", "iPhone Developer")
         options.setCapability("autoAcceptAlerts", true)
